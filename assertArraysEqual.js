6d2453dee503936f39bd684df3d9917113ef96ab
Winnie Li <liwanyi0313@gmail.com>
2022-07-27 04:31:38 +0000

    completed assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+// compare two arrays and returns true of false, based on a perfrct match
+const eqArrays = function(arr1, arr2) {
+  return JSON.stringify(arr1) == JSON.stringify(arr2);
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+// Tests
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
