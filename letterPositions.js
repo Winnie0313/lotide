94d95c16ca9ba62879444a93582e3e18ba62db60
Winnie Li <liwanyi0313@gmail.com>
2022-07-28 00:39:50 +0000

    competed leterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+
+// return all the indices (zero-based positions) in the string where each character is found.
+// return an object: key is the character, value is an array with the indices where the character is found in string
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  // iterate each character in the sentence
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]){
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+        
+      } 
+    }
+    return results;
+  }
+
+
+// compare two arrays and returns true of false, based on a perfrct match
+const eqArrays = function(arr1, arr2) {
+  return JSON.stringify(arr1) == JSON.stringify(arr2);
+}
+
+// return a message to tell if two arrays are the same
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// Test
+
+const string1 = "hello ";
+const result1 = letterPositions(string1);
+assertArraysEqual(result1["h"], [0]);
+assertArraysEqual(result1["e"], [1]);
+assertArraysEqual(result1["l"], [2, 3]);
+assertArraysEqual(result1[" "], undefined);
+
+
+
