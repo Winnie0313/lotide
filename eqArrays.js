d0c693c6e564aa9f0a0ba8626e9d7f081921a18c
Winnie Li <liwanyi0313@gmail.com>
2022-07-28 23:02:03 +0000

    changed == to === in eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 
 // compare two arrays and returns true of false, based on a perfrct match
 const eqArrays = function(arr1, arr2) {
-  return JSON.stringify(arr1) == JSON.stringify(arr2);
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
 // Tests
@@ -18,3 +18,4 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+
