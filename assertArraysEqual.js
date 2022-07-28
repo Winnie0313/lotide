1a344c461ae31fb363bfe93272fddd076b20bd98
Winnie Li <liwanyi0313@gmail.com>
2022-07-28 23:01:05 +0000

    changed == to === in eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,14 @@
 // compare two arrays and returns true of false, based on a perfrct match
 const eqArrays = function(arr1, arr2) {
-  return JSON.stringify(arr1) == JSON.stringify(arr2);
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+// return a message to tell if two arrays are the same
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
