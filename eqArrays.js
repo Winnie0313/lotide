a3752d1e162b4b940d26b808852f15a04333d0ad
Winnie Li <liwanyi0313@gmail.com>
2022-08-01 23:12:54 +0000

    refactored eqArray.js and seperated it's tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-};
 
 // compare two arrays and returns true of false, based on a perfrct match
 const eqArrays = function(arr1, arr2) {
   return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
-// Tests
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-
+module.exports = eqArrays;
