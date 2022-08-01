1a796f77cd185d0795cfbfa255af226aed9ab84a
Winnie Li <liwanyi0313@gmail.com>
2022-08-01 23:00:16 +0000

    refactored tail.js by requiring assertEqual, and exported tail function

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   let result = [];
@@ -26,3 +19,5 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+module.exports = tail;
